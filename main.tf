43045fc8093abedf98c84e49a6318e7c2576c703
Tanguy Rossel <trossel@student.42lausanne.ch>
2023-09-05 13:07:14 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,102 @@
+resource "null_resource" "dependencies" {
+  triggers = var.dependency_ids
+}
+
+resource "argocd_project" "this" {
+  metadata {
+    name      = "<CHART_NAME>"
+    namespace = var.argocd_namespace
+    annotations = {
+      "devops-stack.io/argocd_namespace" = var.argocd_namespace
+    }
+  }
+
+  spec {
+    description  = "<CHART_NAME> application project"
+    source_repos = ["https://github.com/camptocamp/devops-stack-module-<CHART_NAME>.git"]
+
+    destination {
+      name      = "in-cluster"
+      namespace = var.namespace
+    }
+
+    orphaned_resources {
+      warn = true
+    }
+
+    cluster_resource_whitelist {
+      group = "*"
+      kind  = "*"
+    }
+  }
+}
+
+data "utils_deep_merge_yaml" "values" {
+  input = [for i in concat(local.helm_values, var.helm_values) : yamlencode(i)]
+}
+
+resource "argocd_application" "this" {
+  metadata {
+    name      = "<CHART_NAME>"
+    namespace = var.argocd_namespace
+  }
+
+  timeouts {
+    create = "15m"
+    delete = "15m"
+  }
+
+  wait = var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? false : true
+
+  spec {
+    project = argocd_project.this.metadata.0.name
+
+    source {
+      repo_url        = "https://github.com/camptocamp/devops-stack-module-<CHART_NAME>.git"
+      path            = "charts/<CHART_NAME>"
+      target_revision = var.target_revision
+      helm {
+        values = data.utils_deep_merge_yaml.values.output
+      }
+    }
+
+    destination {
+      name      = "in-cluster"
+      namespace = var.namespace
+    }
+
+    sync_policy {
+      dynamic "automated" {
+        for_each = toset(var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? [] : [var.app_autosync])
+        content {
+          prune       = automated.value.prune
+          self_heal   = automated.value.self_heal
+          allow_empty = automated.value.allow_empty
+        }
+      }
+
+      retry {
+        backoff {
+          duration     = "20s"
+          max_duration = "2m"
+          factor       = "2"
+        }
+        limit = "5"
+      }
+
+      sync_options = [
+        "CreateNamespace=true"
+      ]
+    }
+  }
+
+  depends_on = [
+    resource.null_resource.dependencies,
+  ]
+}
+
+resource "null_resource" "this" {
+  depends_on = [
+    resource.argocd_application.this,
+  ]
+}
