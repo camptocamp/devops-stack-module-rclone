0fbb959f28819466b6e7d20d13753aa0a71b4271
Tanguy Rossel <tanguy.rossel@camptocamp.com>
2023-09-20 06:57:56 +0200

    feat: add ingress

--- locals.tf
+++ locals.tf
@@ -11,6 +11,39 @@ locals {
       type = "ClusterIP"
       port = 5572
     }
+
+    ingress = {
+      enabled   = true
+      classname = ""
+      annotations = {
+        "cert-manager.io/cluster-issuer"                   = "ca-issuer" # TODO: Change to letsencrypt-staging
+        "traefik.ingress.kubernetes.io/router.entrypoints" = "websecure"
+        "traefik.ingress.kubernetes.io/router.middlewares" = "traefik-withclustername@kubernetescrd"
+        "traefik.ingress.kubernetes.io/router.tls"         = "true"
+        "ingress.kubernetes.io/ssl-redirect"               = "true"
+        "kubernetes.io/ingress.allow-http"                 = "false"
+      }
+      hosts = [
+      {
+        host = local.domain,
+        paths = [
+          {
+            path     = "/"
+            pathType = "ImplementationSpecific"
+          }
+        ]
+      },
+      {
+        host = local.domain_full,
+        paths = [
+          {
+            path     = "/"
+            pathType = "ImplementationSpecific"
+          }
+        ]
+      }
+      ]
+    }
   }]
 
 }
